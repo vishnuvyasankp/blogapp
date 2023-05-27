9f5f7536a2113e3c49efe4a754c2685dbe0edc9b
vishnuvyasankp <20b128@cev.ac.in>
2023-05-27 16:10:42 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Addform from './pages/Addform';
+
+
+
 export default function App() {
+
   return (
     <>
-    <Routes>
-       <Route path="/" element={<Home />} />
-       <Route path="/Add" element={<Addform />} />
-    </Routes>
- </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Add" element={<Addform />} />
+
+        
+      </Routes>
+    </>
+
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Container from 'react-bootstrap/Container';
 
-export default function home() {
+import Table from 'react-bootstrap/Table';
+
+
+export default function App() {
+  const [todo, setTodo] = useState([]);
+  const getData = async () => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const jsonData = await data.json();
+    setTodo(jsonData)
+  }
+  useEffect(() => {
+    getData();
+
+
+  }, [])
   return (
-    <div>home</div>
+    <div className='mt-5'>
+      <Container className='mt-5'>
+        <h1 className='text-center mt-5'>HOME</h1>
+        <Table striped bordered hover>
+          <thead>
+            <tr className='mt-5'>
+              <th>ID</th>
+              <th>Title</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {
+              todo.map((item, index) => {
+                return(
+                <tr key={index}>
+                  <td>{item.id}</td>
+                  <td>{item.title}</td>
+                </tr>
+                )
+                })}
+          </tbody>
+        </Table>
+      </Container>
+    </div>
   )
 }
