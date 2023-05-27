f8ce071500aec57b3adfcf91935fcdf6ae9892cb
vishnuvyasankp <20b128@cev.ac.in>
2023-05-27 17:05:39 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Addform from './pages/Addform';
-
+import Nav1 from './units/Nav1'
 
 
 export default function App() {
 
   return (
     <>
+    <Nav1></Nav1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Add" element={<Addform />} />
